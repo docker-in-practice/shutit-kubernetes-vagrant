31f56c47cb8b8f4a9b5d68b41e7f9ec68adeb417
Ian Miell <ian.miell@gmail.com>
2016-05-18 09:05:31 +0100

    submodule

--- kubernetes_vagrant.py
+++ kubernetes_vagrant.py
@@ -36,35 +36,37 @@ class kubernetes_vagrant(ShutItModule):
 		# shutit.get_config(module_id,option,default=None) - get configuration value
 		# shutit.get_ip_address() - returns the ip address of the target
 		# shutit.add_line_to_file(line, filename) - add line (or lines in an array) to the filename
-		vagrant_dir = shutit.cfg[self.module_id]['vagrant_dir']
+		vagrant_dir        = shutit.cfg[self.module_id]['vagrant_dir']
 		kubernetes_version = shutit.cfg[self.module_id]['kubernetes_version']
-		num_minions = shutit.cfg[self.module_id]['num_minions']
-		clean_vbox = shutit.cfg[self.module_id]['clean_vbox']
-		master_ip = shutit.cfg[self.module_id]['master_ip']
-		minion_ip_base = shutit.cfg[self.module_id]['minion_ip_base']
+		num_minions        = shutit.cfg[self.module_id]['num_minions']
+		clean_vbox         = shutit.cfg[self.module_id]['clean_vbox']
+		master_ip          = shutit.cfg[self.module_id]['master_ip']
+		minion_ip_base     = shutit.cfg[self.module_id]['minion_ip_base']
+		shutit.send('mkdir -p ' + vagrant_dir)
+		shutit.send('cd ' + vagrant_dir)
 		if clean_vbox:
 			shutit.send("VBoxManage list runningvms | awk '{print $1}'  | xargs -IXXX VBoxManage controlvm XXX poweroff",check_exit=False)
 			shutit.send("VBoxManage list vms | awk '{print $1}'  | xargs -IXXX VBoxManage unregistervm --delete XXX",check_exit=False)
 			shutit.send('''vagrant global-status | grep virtualbox | awk '{print $1}' | xargs vagrant destroy -f''',check_exit=False)
-		shutit.send('mkdir -p ' + vagrant_dir)
-		shutit.send('cd ' + vagrant_dir)
-		if not shutit.file_exists(vagrant_dir + '/kubernetes',directory=True):
+			if shutit.file_exists('kubernetes',directory=True):
+				shutit.send('rm -rf kubernetes')
+		if not shutit.file_exists('kubernetes',directory=True):
 			shutit.send('wget -qO- https://github.com/GoogleCloudPlatform/kubernetes/releases/download/' + kubernetes_version + '/kubernetes.tar.gz | tar -zxf -')
+		shutit.send('cd kubernetes')
 		shutit.send('export KUBERNETES_PROVIDER=vagrant')
 		shutit.send('export NUM_MINIONS=' + num_minions)
 		shutit.send('export MASTER_IP=' + master_ip)
 		shutit.send('export MINION_IP_BASE=' + minion_ip_base)
-		shutit.send('cd kubernetes')
 		if shutit.send_and_match_output('vagrant status',['.*poweroff.*','.*not created.*','.*aborted.*']):
 			shutit.send('./cluster/kube-up.sh',timeout=99999)
-		shutit.pause_point('vagrant status')
+		shutit.pause_point('vagrant status, vagrant ssh if all ok')
 		return True
 
 	def get_config(self, shutit):
 		shutit.get_config(self.module_id, 'vagrant_dir', '/space/vagrant')
 		shutit.get_config(self.module_id, 'kubernetes_version', 'v0.16.2')
 		shutit.get_config(self.module_id, 'num_minions', '2')
-		shutit.get_config(self.module_id, 'clean_vbox', False, boolean=True)
+		shutit.get_config(self.module_id, 'clean_vbox', boolean=True, hint='Whether to wipe out all virtualbox and vagrant boxes')
 		shutit.get_config(self.module_id, 'master_ip','10.245.1.2')
 		shutit.get_config(self.module_id, 'minion_ip_base','10.245.1.3')
 		return True
